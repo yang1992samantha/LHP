37d7abb302838bd5907af0def1813c8fd8d47695
yang1992samantha <347482873@qq.com>
2022-10-20 10:50:52 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,25 @@
 
 data = "medical"
-dataset = "splits/3"
+dataset = "splits/1"
 
 # data = "IAF"
 # dataset = "splits/1"
 
-# data = "rev"
+# data = "uspto"
+# dataset = "splits/1"
+
+
+# data = "iJO1366"
+# dataset = "splits/1"
+
+# data = "our-reverb"
 # dataset = "splits/1"
 
 '''
 gpu: gpu number to use
 cuda: True or False
 '''
-gpu = 3
+gpu = 0
 cuda = True
 epochs = 50000
 
@@ -30,8 +37,9 @@ def parse():
 	p.add_argument('--epochs', type=int, default=epochs, help='number of epochs to train')
 	p.add_argument('--gpu', type=int, default=gpu, help='gpu number to use')
 	p.add_argument('--cuda', type=bool, default=cuda, help='cuda for gpu')
-	p.add_argument('--embed_dim_in', type=int, default=768, help='dim of embedding')
+	p.add_argument('--embed_dim_in', type=int, default=256, help='dim of embedding')#382,256,298,26
 	p.add_argument('--embed_dim_out', type=int, default=512, help='dim of embedding')
+	p.add_argument('--out_dims2', type=int, default=512, help='dim of embedding')
 	p.add_argument('--val_every', type=int, default=1)
 	p.add_argument('--opt', type=str, default="adam")
 	p.add_argument('--lr', type=float, default=0.001)
@@ -40,11 +48,11 @@ def parse():
 	p.add_argument("--loss", type=str, default="new_focal")
 	p.add_argument("--gamma", type=int, default=2)
 
-	p.add_argument("--patience", type=int, default=100)
+	p.add_argument("--patience", type=int, default=3000)
 	#p.add_argument("--task", type=str, default="multi_classifier")
 	p.add_argument("--task", type=str, default="two_classifier")
-	p.add_argument("--embedding", type=str, default="bert")
-	#p.add_argument("--embedding", type=str, default="word2vector")
+	p.add_argument("--embedding", type=str, default="word2vector")
+	#p.add_argument("--embedding", type=str, default="bert")
 	#p.add_argument("--embedding", type=str, default="random")
 	p.add_argument("--rel_mean", type=float, default=10)
 
